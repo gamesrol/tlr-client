a21d17ff0078e9ce22c07f682dc5a5bba22b3a27
gamesrol <gs.gore@gmail.com>
2014-11-16 16:32:53 +0100

    Login Full

--- js/app.js
+++ js/app.js
@@ -3,20 +3,44 @@ $(document).ready(function() {
 });
 
 var urlServer= "http://77.230.212.178:3000";
+var urlServer= "http://0.0.0.0:3000";
+//var urlServer= "http://192.168.1.138:3000";
 
-var app = angular.module('tomalared', ['ngRoute',"ngCookies",'ui.bootstrap','dialogs']);  
 
-app.config(function($routeProvider) {
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match&&match[7].length==11){
+        return match[7];
+    }else{
+        alert("Url incorrecta");
+    }
+}
+
+
+var app = angular.module('tomalared', ['ngRoute','ngCookies','ngResource','ui.bootstrap','dialogs']);  
+
+app.config(function($routeProvider, $httpProvider) {
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+  $httpProvider.defaults.useXDomain = true;
+  
+
   $routeProvider.
     when('/', {
       templateUrl: 'partials/createUser.html',
       controller: 'createUser'
     }).
+    when('/logout', {
+      templateUrl: 'partials/posts.html',
+      controller: 'logout'
+    }).
     when('/posts', {
-      templateUrl: 'partials/login.html',
+      templateUrl: 'partials/posts.html',
       controller: 'posts'
     }).     
     otherwise({
       redirectTo: '/'
     });
 });
+
--- js/controller.js
+++ js/controller.js
@@ -1,58 +1,98 @@
-app.controller('login', function($scope, $http, $cookieStore, $dialogs, $interval) {
+app.controller('login', function($scope, $http, $cookieStore, $dialogs, $interval,$location) {
 	
-	if(!$cookieStore.get('lastValue')){		 
+	if(!$cookieStore.get('user')){		 
 	  	$scope.login = function(){
-			var paco = $http({
-		        method: "POST",
-		        url: urlServer+"/login",        
-		        data: {name: $scope.userName, password: $scope.userPassword},
-		        headers: {'Content-Type': 'application/x-www-form-urlencoded'}        
-		    }).success(function( user ) {	    		
-	            if(!user.errors){		 
+			
+			var dataObject = 'name='+ $('#login_user').val() +'&password='+ $('#login_password').val();
+			var responsePromise = $http.post(urlServer+"/login", dataObject);
+	       	
+	       	responsePromise.success(function(user, status, headers, config) {
+	        	if(!user.errors){		 	        		
 	            	$cookieStore.put('user', user);           			            		            		
-	            	$location.path("/posts");
-	            	console.log(user);
+	               	location.reload();         			            		            		
 	            }else{
-	            	$dialogs.error('Error en el logeo');
-	            	$cookieStore.remove(user);           			            		            		
-	            }		           		            	           	
-		    }).error(function( user ) {
-	            $dialogs.error("Error en la conexion");
-		    });								    
-	  	};	  	
+	            	$dialogs.error("Error en el logeo");
+	            	$cookieStore.remove(user); 	            	  			            		            		
+	            }	
+	       	});
+	        
+	        responsePromise.error(function(data, status, headers, config) {
+	          	 $dialogs.error("Error en la conexion");
+	       	});
+
+        };	
+
 	 	$scope.athu = false;
   	}else{  		
+		$scope.logout = function(){				
+			$cookieStore.remove('user');  				
+			location.reload();         			            		            		  		
+        };	
   		$scope.athu = true;
   	}		
 	
 	var is_connected = function(){
 		$http({
-	        method: "POST",
-	        url: urlServer+"/login",        
-	        data: {name: null, password: null},
-	        headers: {'Content-Type': 'application/x-www-form-urlencoded'}        
-	    }).success(function() {	    		
-			$scope.connection = true;  
+	        method: "GET",
+	        url: urlServer+"/version"
+	    }).success(function() {
+			$scope.connection = true;
 			$cookieStore.put("connection", true);
 	    }).error(function() {
            	$scope.connection = false;
            	$cookieStore.put("connection", false);
 	   	});
-	}
-
-	is_connected();
-	$interval(is_connected(), 50);
+	}	
+	$interval(is_connected(), 500);
 });
 
-app.controller('createUser', function($scope, $http, $cookieStore,$dialogs) {
+app.controller('createUser', function($scope, $http, $cookieStore,$dialogs, $location) {
+	if(!$cookieStore.get('user')){				
+		$scope.create= function(){	  		
+	  		var dataObject = 
+		        	'name='+ $scope.user.name +
+		        	'&profile='+ $scope.user.profile +
+		        	'&email='+ $scope.user.email +
+		        	'&password='+ $scope.user.password +
+		        	'&password_confirmation='+ $scope.user.passwordConfirmation +
+		        	'&accept='+ $scope.user.accept;
+	  		var responsePromise = $http.post(urlServer+"/signup", dataObject);
+			
+			responsePromise.success(function( user ) {	    		
+				if(user.message == "Cuenta creada!!"){		 
+	            	$dialogs.notify(user.message, user.message);
+	            	$scope.user = {};
+	            }else{
+	            	$dialogs.error(user.message);	            	           			            		            		
+	            }
+		    });
+
+		    responsePromise.error(function( user ) {
+	            $dialogs.error("Error en la conexion");
+		    });								    
+	  	};
+  	}else{
+  		$location.path("/posts");  		
+  	}		
 });
 
-app.controller('post', function($scope, $http) {
-	if(!$cookieStore.user){		 
+app.controller('posts', function($scope, $http, $cookieStore, $location) {
+	
+	if(!$cookieStore.get('user')){		 
 	  	$location.path("/");  		
   	}else{
-  		
-  	}		
+  		var user = $cookieStore.get('user');  		  		
+		var responsePromise = $http.get(urlServer+"/posts/"+user.id);
+
+		responsePromise.success(function(posts) {	    
+			for (post in posts) { 
+				post.content = "sdafdf";
+				//String(post.content).replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="200" height="100" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '<iframe src="//player.vimeo.com/video/$1" width="200" height="100" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+			}				
+			$scope.posts = posts;			
+		});
+  	}  	
+
 });
 
 
