82052cb5d3f4720306397adb2a4d1154992ba758
Gamesrol <gs.gore@gmail.com>
2014-11-22 19:49:55 +0100

    User edit beta

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
 var urlServer= "http://77.230.212.178:3000";
 var urlServer= "http://217.217.60.111:3000";
 //var urlServer= "http://192.168.1.138:3000";
+//var urlServer= "http://0.0.0.0:3000";
 
-var app = angular.module('tomalared', ['ngRoute','ngCookies','ngResource','ui.bootstrap','dialogs']);  
+var app = angular.module('tomalared', ['ngRoute','ngCookies','ngResource','ui.bootstrap','dialogs','infinite-scroll']);  
 
 app.filter("postsParse", function($sce){
   return function(text){
@@ -16,21 +17,23 @@ app.filter("postsParse", function($sce){
     var ytembed = '<iframe src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>';    
     var vimurl = /(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g;
     var vimembed = '<iframe src="//player.vimeo.com/video/$1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-    var aurl = /(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/;
-    var aembed = '<a href="$1">$1</a>';
-    var imgurl = /^(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])(.png|.jpg|.gif)$/gim;
-    var imgembed = '<img src="$1$3">'
+    var tagurl = /(^#(.+))/;
+    var tagembed = '<a href="#/tag/$2">$1</a>';
+    var userurl = /(^@(.+))/;
+    var userembed = '<a href="#/user/$2">$1</a>';
+    var imgurl = /((https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|](.png|.jpg|.jpeg|.gif|.PNG|.JPG|.JPEG|.GIF))$/gim;
+    var imgembed = '<img src="$1">'
 
-    
+    // /^#/
     for (x=0;x<words.length;x++){            
-      textFinal += words[x].replace(yturl, ytembed).replace(vimurl, vimembed).replace(aurl, aembed).replace(imgurl, imgembed);          
+      textFinal += words[x].replace(yturl, ytembed).replace(vimurl, vimembed).replace(imgurl, imgembed).replace(tagurl, tagembed).replace(userurl, userembed)+" ";          
     }
 
     replacePattern1 = /^(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
     replacedText = textFinal.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
     replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacePattern3 = /^(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
     replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
     
     return $sce.trustAsHtml(replacedText);
@@ -47,14 +50,22 @@ app.config(function($routeProvider, $httpProvider) {
       templateUrl: 'partials/createUser.html',
       controller: 'createUser'
     }).
-    when('/logout', {
-      templateUrl: 'partials/posts.html',
-      controller: 'logout'
+    when('/profile', {
+      templateUrl: 'partials/editUser.html',
+      controller: 'editUser'
     }).
     when('/posts', {
       templateUrl: 'partials/posts.html',
       controller: 'posts'
-    }).     
+    }).
+    when('/tag/:tag', {
+      templateUrl: 'partials/posts.html',
+      controller: 'posts'
+    }).
+    when('/user/:user', {
+      templateUrl: 'partials/posts.html',
+      controller: 'posts'
+    }).      
     otherwise({
       redirectTo: '/'
     });
--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,5 @@
-app.controller('login', function($scope, $http, $cookieStore, $dialogs, $interval,$location) {
+/* User Controller */
+app.controller('login', function($scope, $http, $cookieStore, $dialogs, $modal, $interval,$location) {
 	
 	if(!$cookieStore.get('user')){		 
 	  	$scope.login = function(){
@@ -29,6 +30,19 @@ app.controller('login', function($scope, $http, $cookieStore, $dialogs, $interva
 			location.reload();         			            		            		  		
         };	
   		$scope.athu = true;
+
+  	  	$scope.openPub = function () {
+	        $modal.open({
+	            templateUrl: 'partials/publisher.html',
+	            backdrop: true,
+	            windowClass: 'modal',
+	            controller: 'publisher',            
+	        });
+    	};
+
+    	$scope.profile = function (){
+    		$location.path("/profile");  
+    	}
   	}		
 	
 	var is_connected = function(){
@@ -76,19 +90,63 @@ app.controller('createUser', function($scope, $http, $cookieStore,$dialogs, $loc
   	}		
 });
 
-app.controller('posts', function($scope, $http, $cookieStore, $location) {
-	if(!$cookieStore.get('user')){		 
-	  	$location.path("/");  		
-  	}else{
-  		var user = $cookieStore.get('user');  		  		
-		var responsePromise = $http.get(urlServer+"/posts/"+user.id);
-
-		responsePromise.success(function(posts) {	    
-			//String(post.content).replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="200" height="100" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '<iframe src="//player.vimeo.com/video/$1" width="200" height="100" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
-			$scope.posts = posts;			
-		});
-  	}  	
-
+app.controller('editUser', function($scope, $http, $cookieStore, $location,$route) {	
+	$scope.user = $cookieStore.get('user');
+	$scope.edit = function(){
+		/*var dataObject = 'content='+ encodeURIComponent($scope.post.text)+"&user_id="+$scope.user.id;
+	  	var responsePromise = $http.post(urlServer+"/pub", dataObject);
+		$modalInstance.dismiss();
+		responsePromise.success(function( json ) {	    		
+			$location.path("/posts"); 
+			$route.reload(); 
+			console.log(json);
+		});*/		
+		console.log($scope.user);
+	}	
 });
 
 
+/* Post Controller */
+
+app.controller('publisher', function($scope, $http, $cookieStore, $location,$modalInstance,$route) {	
+	$scope.user = $cookieStore.get('user');
+	$scope.pub = function(){
+		var dataObject = 'content='+ encodeURIComponent($scope.post.text)+"&user_id="+$scope.user.id;
+	  	var responsePromise = $http.post(urlServer+"/pub", dataObject);
+		$modalInstance.dismiss();
+		responsePromise.success(function( json ) {	    		
+			$location.path("/posts"); 
+			$route.reload(); 
+			console.log(json);
+		});
+		
+	}	
+});
+
+app.controller('posts', function($scope, $http, $cookieStore, $location, $routeParams) {
+	if(!$cookieStore.get('user')){		 
+	  	$location.path("/");  		
+  	}else{
+  		var user = $cookieStore.get('user');
+
+		if ($routeParams.tag){
+			var responsePromise = $http.get(urlServer+"/postsTag/"+$routeParams.tag);	
+			responsePromise.success(function(posts) {	    
+				//String(post.content).replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="200" height="100" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '<iframe src="//player.vimeo.com/video/$1" width="200" height="100" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+				$scope.posts = posts;			
+			});
+		}else if($routeParams.user){
+			var responsePromise = $http.get(urlServer+"/posts/"+$routeParams.user);	
+			responsePromise.success(function(posts) {	    
+				//String(post.content).replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="200" height="100" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '<iframe src="//player.vimeo.com/video/$1" width="200" height="100" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+				$scope.posts = posts;			
+			});
+		}else{
+			var responsePromise = $http.get(urlServer+"/posts/"+user.name);	
+			responsePromise.success(function(posts) {	    
+				//String(post.content).replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="200" height="100" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '<iframe src="//player.vimeo.com/video/$1" width="200" height="100" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+				$scope.posts = posts;			
+			});
+		}
+  	}  	
+});
